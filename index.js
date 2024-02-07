548c4fac85f16d1fe5c1bae1927d6e9ebf387212
SAIKV2000 <vangalaskwins@gmail.com>
2024-02-07 20:17:35 +0530

    node

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require("express")
+let bodyparser = require("body-parser")
+
+const route = require("./routes/route")
+const app = express()
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.use("/",route)
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
